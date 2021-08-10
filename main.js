88e6c757c28bebcd72e1b65150600b0996b3811e
WON-JIN-LEE <vopgh0409@gmail.com>
2021-08-10 21:35:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.querySelector(toggleId),
+        nav = document.querySelector(navId);
+    
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show');
+        });
+    }
+}
+showMenu('#nav-toggle','#nav-menu')
